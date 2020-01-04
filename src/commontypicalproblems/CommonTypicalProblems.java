5c686874dcee200d40670f3d7ac5f9ec2d8d4001
Yan <Yan@dyn3173-135.wlan.ic.ac.uk>
2020-01-04 17:01:40 +0000

    Word Ladder

--- src/commontypicalproblems/CommonTypicalProblems.java
+++ src/commontypicalproblems/CommonTypicalProblems.java
@@ -34,6 +34,40 @@ public class CommonTypicalProblems {
         return true;
     }
 
+    //Word ladder:
+    public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = new HashSet<>(wordList);
+        if (beginWord == null || beginWord.isEmpty() ||
+                endWord == null || endWord.isEmpty())
+            return 0;
+        int length = 1;
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String curr = queue.poll();
+                for (int j = 0; j < curr.length(); j++) {
+                    char[] charCurr = curr.toCharArray();
+                    for (char c = 'a'; c < 'z'; c++) {
+                        charCurr[j] = c;  // change one character at a time
+                        String strCurr = new String(charCurr);
+                        if (dict.contains(strCurr)) {
+                            if (strCurr.equals(endWord)) {
+                                return length + 1;
+                            } else {
+                                queue.offer(strCurr);
+                                dict.remove(strCurr);
+                            }
+                        }
+                    }
+                }
+            }
+            length++;
+        }
+        return 0;
+    }
+
     //Find the area of the largest rectangle in a histogram:
     public static int largestRectangleArea(int[] heights) {
         if (heights == null || heights.length == 0){
