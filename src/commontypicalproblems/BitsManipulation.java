8e84a98f0590f7b0f43074b07b377dce620402c4
Yan <Yan@dyn3161-9.wlan.ic.ac.uk>
2020-01-07 21:04:09 +0000

    Bits and Insertion

--- src/commontypicalproblems/BitsManipulation.java
+++ src/commontypicalproblems/BitsManipulation.java
@@ -1,4 +1,14 @@
 package commontypicalproblems;
 
 public class BitsManipulation {
+
+    //In a list, every number appears twice except one number. Find the unique number:
+    public static int singleNumber(int[] nums) {
+        int n = 0;
+        for (int num : nums) {
+            n ^= num;
+        }
+        return n;
+    }
+
 }
---
+++ src/commontypicalproblems/Sorting.java
@@ -0,0 +1,17 @@
+package commontypicalproblems;
+
+public class Sorting {
+
+    //Insertion sort (in-place):
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+    }
+}
